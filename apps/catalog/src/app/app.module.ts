2aa7e08fb1eae1744d9234ee119b54b24eccd46b
Luiz Alberto <luiz010321@gmail.com>
2025-06-30 15:19:13 -0300

    feat(cache): criação de provider de cache, abstração generica e tambem implementacao usando redis

--- apps/catalog/src/app/app.module.ts
+++ apps/catalog/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppController } from './app.controller';
 import { ProductRepository } from './domain/repositories/product.repository';
 
 import { ProductMySqlRepository } from './infrastructure/repositories/product-mysql.repository';
+import { CacheProvider } from './infrastructure/cache/cache.provider';
+import { RedisCacheProvider } from './infrastructure/cache/redis-cache.provider';
 
 import { GetProductsUseCase } from './application/use-cases/get-products.use-case';
 import { GetProductBySlugUseCase } from './application/use-cases/get-product-by-slug.use-case';
@@ -24,6 +26,10 @@ import { GetProductsAutocompleteUseCase } from './application/use-cases/get-prod
     {
       provide: ProductRepository,
       useClass: ProductMySqlRepository,
+    },
+    {
+      provide: CacheProvider,
+      useClass: RedisCacheProvider,
     },
     GetProductsUseCase,
     GetProductBySlugUseCase,
